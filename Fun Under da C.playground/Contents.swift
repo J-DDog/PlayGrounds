2645528df54d7d16629e9616be0e3331201bb05a
Traitortots <jker3169@740s-pl-122.local>
2015-12-07 08:48:52 -0700

    created a function

--- Fun Under da C.playground/Contents.swift
+++ Fun Under da C.playground/Contents.swift
@@ -2,4 +2,40 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+//------------------------------
+var simple = "💩😨, 😰🔫"
+
+//------------------------------
+
+for letter in simple
+{
+    print(letter)
+}
+//------------------------------
+
+
+func simpleMethoud(numberOfThings :Int) -> Int
+{
+    if(numberOfThings > 5)
+    {
+        
+        return 100
+    }
+    else if(numberOfThings < -20)
+    {
+        
+        return -20
+    }
+    else
+    {
+        
+        return numberOfThings
+    }
+
+}
+    
+
+
+print(simpleMethoud(5))
+print(simpleMethoud(-48))
+
